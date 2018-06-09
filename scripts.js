aea826719cb0f7852387601464a25d81a2b076d8
Max Heffern <mrheffern@gmail.com>
2018-06-09 16:49:09 -0700

    added first recipe

---
+++ scripts.js
@@ -0,0 +1,37 @@
+int recipes = {
+  cheesey chicken : {
+    ingrediant1 : {
+      "name" : "rice",
+      "amount" : 0.25,
+      "units" : "cup(s)"
+    }
+
+    ingrediant2 : {
+      "name" : "chicken",
+      "amount" : 1,
+      "units" : "breast(s)"
+    }
+
+    ingrediant3 : {
+      "name" : "cheese soup",
+      "amount" : 0.5,
+      "units" : "can(s)"
+    }
+
+    ingrediant4 : {
+      "name" : "milk",
+      "amount" : 0.25,
+      "units" : "cup(s)"
+    }
+
+    ingrediant5 : {
+      "name" : "pepper",
+      "amount" : "to taste",
+    }
+
+    ingrediant6 : {
+      "name" : "salt",
+      "amount" : "to taste"
+    }
+  }
+};
