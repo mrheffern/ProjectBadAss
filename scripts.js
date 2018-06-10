5bcb7fec00a66890904d25cca5c7e15257c3a267
Max Heffern <mrheffern@gmail.com>
2018-06-09 17:16:57 -0700

    nested individual ingredients in new ingredients object

--- scripts.js
+++ scripts.js
@@ -4,38 +4,41 @@ int recipes = {
 
     healthy : false,
 
-    ingredient1 : {
-      name : "rice",
-      amount : 0.25,
-      units : "cup(s)"
-    },
+    ingredients : {
 
-    ingredient2 : {
-      name : "chicken",
-      amount : 1,
-      units : "breast(s)"
-    },
+      ingredient1 : {
+        name : "rice",
+        amount : 0.25,
+        units : "cup(s)"
+      },
 
-    ingredient3 : {
-      name : "cheese soup",
-      amount : 0.5,
-      units : "can(s)"
-    },
+      ingredient2 : {
+        name : "chicken",
+        amount : 1,
+        units : "breast(s)"
+      },
 
-    ingredient4 : {
-      name : "milk",
-      amount : 0.25,
-      units : "cup(s)"
-    },
+      ingredient3 : {
+        name : "cheese soup",
+        amount : 0.5,
+        units : "can(s)"
+      },
 
-    ingredient5 : {
-      name : "pepper",
-      amount : "to taste"
-    },
+      ingredient4 : {
+        name : "milk",
+        amount : 0.25,
+        units : "cup(s)"
+      },
 
-    ingredient6 : {
-      name : "salt",
-      amount : "to taste"
+      ingredient5 : {
+        name : "pepper",
+        amount : "to taste"
+      },
+
+      ingredient6 : {
+        name : "salt",
+        amount : "to taste"
+      }
     }
   }
 };
