0344e7ed7f71c9d418ff9ac1aab8da2ee48f8178
Max Heffern <mrheffern@gmail.com>
2018-07-09 13:23:04 -0700

    added comments to javascript

--- scripts.js
+++ scripts.js
@@ -45,20 +45,20 @@ var recipes = {
 
 var testLog = "Woops";
 var amountWithUnits = "";
-
+// loops through each ingredient in cheesyChicken
 for (var key in recipes.cheesyChicken.ingredients) {
+  // prints name of ingredient to console
   testLog = "Name: " + recipes.cheesyChicken.ingredients[key]["name"];
   console.log(testLog);
-
+  // saves amount of ingredient needed
   testLog = "Amount: " + recipes.cheesyChicken.ingredients[key]["amount"];
-
+  // checks if ingredient has "units" property. if so units is appened to "testLog"
   if (recipes.cheesyChicken.ingredients[key].hasOwnProperty("units")) {
     testLog = testLog + " " + recipes.cheesyChicken.ingredients[key]["units"];
   }
-
+  // prints amount of ingredient and units
   console.log(testLog);
 };
 console.log("damn dog");
 //loop through recipe --> loop ingrients --> loop ingredientX
 //store name --> if there is units conc with amount --> display
-
