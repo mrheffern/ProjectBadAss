05b46a7f939563cac0af50baafe1a4b7207f51d2
Max Heffern <mrheffern@gmail.com>
2018-08-04 23:44:31 -0700

    I don't even know what changes I made or how to use Git...

--- scripts.js
+++ scripts.js
@@ -1,107 +1,3 @@
-var recipes = {
-  scrambledEggs : {
-    name : "Scrambled Eggs",
-    healthy : true,
-    ingredients : {
-      ingredient1 : {
-        name : "eggs",
-        amount : 2
-      },
-
-      ingredient2 : {
-        name : "milk",
-        amount : "to taste"
-      }
-    }
-  },
-
-  cheese : {
-    name : "Scrambled Eggs",
-    healthy : true,
-    ingredients : {
-      ingredient1 : {
-        name : "eggs",
-        amount : 2
-      }
-    }
-  },
-
-  chickenRiceBowls : {
-    name : "Chicken Rice Bowls",
-    healthy : true,
-    ingredients : {
-      ingredient1 : {
-        name : "chicken",
-        amount : 1,
-        units : "breast(s)"
-      }
-    }
-  },
-
-  cereal : {
-    name : "cereal",
-
-    healthy : false,
-
-    ingredients : {
-
-      ingredient1 : {
-        name : "cereal",
-        amount: 1,
-        units: "bowl(s)"
-      },
-
-      ingredient2 : {
-        name: "milk",
-        amount: 3,
-        units: "cup(s)"
-      }
-    }
-  },
-
-  cheesyChicken : {
-    name : "Cheesy Chicken",
-
-    healthy : false,
-
-    ingredients : {
-
-      ingredient1 : {
-        name : "rice",
-        amount : 0.25,
-        units : "cup(s)"
-      },
-
-      ingredient2 : {
-        name : "chicken",
-        amount : 1,
-        units : "breast(s)"
-      },
-
-      ingredient3 : {
-        name : "cheese soup",
-        amount : 0.5,
-        units : "can(s)"
-      },
-
-      ingredient4 : {
-        name : "milk",
-        amount : 0.25,
-        units : "cup(s)"
-      },
-
-      ingredient5 : {
-        name : "pepper",
-        amount : "to taste"
-      },
-
-      ingredient6 : {
-        name : "salt",
-        amount : "to taste"
-      }
-    }
-  }
-};
 
 
 var x = 1;
@@ -145,7 +41,136 @@ for (var listItem in shoppingList) {
   console.log(listItem + " " + shoppingList[listItem]);
 }
 
+function createRecipes() {
+  var recipes = {
+    scrambledEggs : {
+      name : "Scrambled Eggs",
+      healthy : true,
+      ingredients : {
+        ingredient1 : {
+          name : "eggs",
+          amount : 2
+        },
 
+        ingredient2 : {
+          name : "milk",
+          amount : "to taste"
+        }
+      }
+    },
+
+    cheese : {
+      name : "Scrambled Eggs",
+      healthy : true,
+      ingredients : {
+        ingredient1 : {
+          name : "eggs",
+          amount : 2
+        }
+      }
+    },
+
+    chickenRiceBowls : {
+      name : "Chicken Rice Bowls",
+      healthy : true,
+      ingredients : {
+        ingredient1 : {
+          name : "chicken",
+          amount : 1,
+          units : "breast(s)"
+        }
+      }
+    },
+
+    cereal : {
+      name : "cereal",
+
+      healthy : false,
+
+      ingredients : {
+
+        ingredient1 : {
+          name : "cereal",
+          amount: 1,
+          units: "bowl(s)"
+        },
+
+        ingredient2 : {
+          name: "milk",
+          amount: 3,
+          units: "cup(s)"
+        }
+      }
+    },
+
+    cheesyChicken : {
+      name : "Cheesy Chicken",
+
+      healthy : false,
+
+      ingredients : {
+
+        ingredient1 : {
+          name : "rice",
+          amount : 0.25,
+          units : "cup(s)"
+        },
+
+        ingredient2 : {
+          name : "chicken",
+          amount : 1,
+          units : "breast(s)"
+        },
+
+        ingredient3 : {
+          name : "cheese soup",
+          amount : 0.5,
+          units : "can(s)"
+        },
+
+        ingredient4 : {
+          name : "milk",
+          amount : 0.25,
+          units : "cup(s)"
+        },
+
+        ingredient5 : {
+          name : "pepper",
+          amount : "to taste"
+        },
+
+        ingredient6 : {
+          name : "salt",
+          amount : "to taste"
+        }
+      }
+    }
+  };
+  return recipes;
+}
+
+function createItemList(recipes) {
+  var x = 1;
+  var shoppingList = {};
+  // loops through each recipe
+  for (var item in recipes){
+    // loops through each ingredient in current recipe
+    for (var key in recipes[item].ingredients) {
+      var currentIngredientName = recipes[item].ingredients[key].name;
+      var currentIngredientAmount = recipes[item].ingredients[key].amount;
+      shoppingList[currentIngredientName] = currentIngredientAmount;
+    }
+  }
+      // checks to see if item already exist on shopping list
+}
+
+function createGroceryList(itemList) {
+
+}
+
+function main() {
+  createGroceryList(createItemList(createRecipes()));
+}
 
 // grocerryList = {
 //  item1 : {
